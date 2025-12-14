2681c4fc63b0caab357def497da2bef72614b258
Ваше Имя <@.>
2025-12-14 22:43:15 +0600

    fixed bugs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/sakiev24/cinemascope', // Replace 'Letterboxd' with your actual GitHub repo name
+  base: '/cinemascope/', // Replace 'Letterboxd' with your actual GitHub repo name
 })
