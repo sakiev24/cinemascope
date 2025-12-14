ac95fe49f36fb8c9e3809c23ce44cd1eb95c5477
Ваше Имя <@.>
2025-12-14 23:28:32 +0600

    Fix deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cinemascope/', // Replace 'Letterboxd' with your actual GitHub repo name
+  base: './', // Replace 'Letterboxd' with your actual GitHub repo name
 })
